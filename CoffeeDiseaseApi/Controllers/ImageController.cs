dc5e5ed87b0c5ef7922b8ada8783031c529cff38
Kim Ariel Dequilla <deqkim4@gmail.com>
2024-03-11 01:52:27 +0800

    feat: train dataset

--- CoffeeDiseaseApi/Controllers/ImageController.cs
+++ CoffeeDiseaseApi/Controllers/ImageController.cs
@@ -11,34 +11,37 @@ namespace CoffeeDiseaseApi.Controllers
         [Consumes("multipart/form-data")]
         public async Task<IActionResult> Predict([FromForm] ImageInputDto input)
         {
-            if (input == null || input.Image == null || input.Image.Length == 0)
-            {
-                Console.WriteLine("Test");
-                return BadRequest("Image file is required.");
+            Console.WriteLine("Reach");
+            try {
+                if (input == null || input.Image == null || input.Image.Length == 0)
+                {
+                    Console.WriteLine("Test");
+                    return BadRequest("Image file is required.");
+                }
+
+                // Read image bytes
+                byte[] imageBytes;
+                using (var memoryStream = new MemoryStream())
+                {
+                    await input.Image.CopyToAsync(memoryStream);
+                    imageBytes = memoryStream.ToArray();
+                }
+                // Now you can use 'imageBytes' for further processing
+
+                //Load sample data
+                CoffeeDiseaseModel.ModelInput sampleData = new()
+                {
+                    ImageSource = imageBytes,
+                };
+
+                //Load model and predict output
+                var result = CoffeeDiseaseModel.Predict(sampleData);
+
+                return Ok(new Response { ClassName = result.PredictedLabel, Score = Math.Round(result.Score.Max() * 100, 2) });
             }
-
-            // Read image bytes
-            byte[] imageBytes;
-            using (var memoryStream = new MemoryStream())
-            {
-                await input.Image.CopyToAsync(memoryStream);
-                imageBytes = memoryStream.ToArray();
+            catch(Exception ex) {
+                return BadRequest(ex.Message);
             }
-
-            // Now you can use 'imageBytes' for further processing
-
-            //Load sample data
-            CoffeeDiseaseModel.ModelInput sampleData = new()
-            {
-                ImageSource = imageBytes,
-            };
-
-            //Load model and predict output
-            var result = CoffeeDiseaseModel.Predict(sampleData);
-
-
-
-            return Ok(new Response { ClassName = result.PredictedLabel, Score = Math.Round(result.Score.Max() * 100, 2) });
         }
     }
     public record Response
@@ -46,8 +49,10 @@ namespace CoffeeDiseaseApi.Controllers
         public string ClassName { get; init; } = null!;
         public object Score { get; init; } = null!;
     }
+
     public class ImageInputDto
     {
         public IFormFile Image { get; set; } = null!;
     }
+
 }
